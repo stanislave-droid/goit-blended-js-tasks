f6c1b54b72755df5599c911bcc8874066ae4ca1f
Stanislave <stasdroner@gmail.com>
2026-03-02 20:45:17 +0200

    Add version 2 of solve to task 5

--- js/task5.js
+++ js/task5.js
@@ -1,6 +1,7 @@
 const login = ["admin", "common"];
 const password = ["admin", "common"];
 
+// Version 1
 function checkRegistration() {
   let inputLogin = prompt("Pls, write your login: ");
   for (let i = 0; i < login.length; i++) {
@@ -16,4 +17,22 @@ function checkRegistration() {
   return "Wrong login";
 }
 
+//Version 2
+
+function checkRegistration2() {
+  let inputLogin = prompt("Pls, write your login: ");
+  const index = login.indexOf(inputLogin);
+  if (index >= 0) {
+    let inputPassword = prompt("Pls, write your password: ");
+    if (password[index] == inputPassword) {
+      return `Hi ${inputLogin}`;
+    } else {
+      return "Wrong password";
+    }
+  } else {
+    return "Wrong login";
+  }
+}
+
 alert(checkRegistration());
+alert(checkRegistration2());
